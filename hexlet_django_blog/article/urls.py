f45ae37b5bc748c65fb15e7312e2ec31d942be03
OlegKhiretdinov <oleg@oleg.win>
2023-11-28 10:33:12 +0300

    feat: функциональные вьюхи заменены на классовые

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from hexlet_django_blog.article import views
 
 urlpatterns = [
-    path('', views.index),
+    path(
+        '<str:tag>/<int:article_id>',
+        views.ArticlesListView.as_view(),
+        name='article'
+    ),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,8 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 
-def index(request):
-    return render(request, 'articles/index.html', context={
-        'title': 'Articles',
-    })
+# def index(request):
+#     return render(request, 'articles/index.html', context={
+#         'title': 'Articles',
+#     })
+
+
+class ArticlesListView(View):
+
+    def get(self, request, tag, article_id,  *args, **kwargs):
+        # return HttpResponse('Hello, World!')
+
+        return render(request, 'articles/index.html', context={
+                'title': 'Articles',
+                'article_id': article_id,
+                'tag': tag,
+            })
+
